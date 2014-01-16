c8cf0b30ca646247513f28e5dd67d34e114d11cc
Eric Heidelmark <eric@sportstechinc.com>
2014-01-16 09:53:34 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DraftOrder::Application.config.secret_key_base = 'c6cfc92b4b52797264943bc6f2b659d1f17f3bf8021c1b3843c4b817f2215db9a94b4809964e13be109cef73f45aac193161a9b477a3e0113cd3ec133c0bbb2b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DraftOrder::Application.config.session_store :cookie_store, key: '_draft_order_session'
